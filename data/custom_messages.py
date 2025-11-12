935e0e0552e0ebfe7098d1520b3abe35cdbe5f1d
Andrew <andrewgv31@yandex.ru>
2025-11-12 15:31:02 +0300

    Рефактор структуры сообщений и кнопок. Поддержка RU/EN
    Заменил старые стартовые сообщения и кнопки на новую структуру с поддержкой разных языков.
    
    Добавил новые JSON-файлы с сообщениями и кнопками на русском языке.

---
+++ data/custom_messages.py
@@ -0,0 +1,8 @@
+
+
+def load_custom_message(message_id):
+    text = ""
+    if message_id == "test":
+        text = "aeee"
+
+    return text
--- src/core/loader.py
+++ src/core/loader.py
@@ -17,33 +17,46 @@ def load_file(path):
         return None
 
 class Message:
-    def __init__(self, message_id):
-        data = self.load_message(message_id)
+    def __init__(self, message_path):
+        data, message_id = self.load_message(message_path)
+        if not data: raise FileNotFoundError(f'Файл сообщения {message_path}.json не найден.')
 
-        self.text = data["text"]
+        if data["text"] != "custom":
+            self.text = data["text"]
+        else:
+            from data.custom_messages import load_custom_message
+            self.text = load_custom_message(message_id)
 
         self.builder = InlineKeyboardBuilder()
 
-        for row, buttons in data["buttons"].items():
-            button_objs = [Button(button_id) for button_id in buttons]
+        for row, buttons in data.get("buttons", {}).items():
+            button_objs = []
+            for button_id in buttons:
+                button_objs.append(Button(button_id))
+
+            if not button_objs:
+                continue
+
             callback_buttons = [CallbackButton(text=b.title, payload=b.message) for b in button_objs]
 
             self.builder.row(*callback_buttons)
 
 
-    def load_message(self, message_id):
+    def load_message(self, message_path):
         path = 'data/messages'
-        path = os.path.join(path, f'{message_id}.json')
-        return load_file(path)
+        path = os.path.join(path, f'{message_path}.json')
+        return load_file(path), os.path.basename(path)
 
 
 class Button:
-    def __init__(self, button_id):
-        data = self.load_button(button_id)
+    def __init__(self, button_path):
+        data = self.load_button(button_path)
+        if not data: raise FileNotFoundError(f'Файл кнопки {button_path}.json не найден')
+
         self.title = data["title"]
         self.message = data["message"]
 
-    def load_button(self, button_id):
+    def load_button(self, button_path):
         path = 'data/buttons'
-        path = os.path.join(path, f'{button_id}.json')
+        path = os.path.join(path, f'{button_path}.json')
         return load_file(path)
